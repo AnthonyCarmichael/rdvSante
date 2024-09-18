744c1a9299cde7146e8ad7d0c29c2f589499ac41
Maxime Malette <132301458+madmax9845@users.noreply.github.com>
2024-09-18 18:46:34 -0400

    Merge main

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -19,6 +19,198 @@
         <header>
             <!-- Logo -->
             <nav class="bg-dark-green p-2">
+                <div class="flex text-white items-center ml-6">
+                    <img class="w-[5rem]" src="{{ asset('img/logoRdvSante.png') }}" alt="Logo">
+                    <h1 class="ml-6 text-2xl font-extrabold">Rendez-vous santé</h1>
+                </div>
+            </nav>
+        </header>
+
+        <div class="relative min-h-screen md:flex" x-data="{ open: true}">
+
+            <!-- Sidebar -->
+            <!-- Ajouter -translate-x-full en JS pour fermer le Sidebar -->
+            <aside :class="{ '-translate-x-full': !open }" class="bg-mid-green z-10 text-darker-green absolute inset-y-0 left-0 md:relative
+                transform md:translate-x-0 overflow-y-auto transition ease-in-out duration-200 shadow-lg">
+
+                <div class="flex justify-end">
+                    <button type="button" @click="open = !open" class="md:hidden inline-flex justify-center items-center rounded-md absolute bg-pale-green
+                        hover:bg-white focus:outline-none">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="block size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                        </svg>
+                    </button>
+                </div>
+
+                <!-- nav links -->
+                <nav>
+                    <ul>
+                        <li class="block {{ (Route::is('index')) ? 'text-white bg-green' : ''}} nav-item p4">
+                            <a class="hover:text-white" href="{{ route('index')}}">
+                                <div class="flex justify-center">
+                                    <button class="">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                            class="size-10 hover">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                                        </svg>
+                                    </button>
+                                </div>
+                                <p class="text-center">Accueil</p>
+                            </a>
+                        </li>
+
+                        <li class="block {{ (Route::is('')) ? 'text-white bg-green' : ''}} nav-item p4">
+                            <a class="hover:text-white" href="">
+                                <div class="flex justify-center">
+                                    <button class="">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                            class="size-10">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                        </svg>
+
+                                    </button>
+                                </div>
+
+                                <p class="text-center">Profil</p>
+                            </a>
+                        </li>
+
+                        <li class="block {{ (Route::is('')) ? 'text-white bg-green' : ''}} nav-item p4">
+                            <a class="hover:text-white" href="">
+                                <div class="flex justify-center">
+                                    <button class="">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                            class="size-10">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
+                                        </svg>
+                                    </button>
+                                </div>
+                                <p class="text-center">Client</p>
+                            </a>
+                        </li>
+
+                        <li class="block {{ (Route::is('')) ? 'text-white bg-green' : ''}} nav-item p4">
+                            <a class="hover:text-white" href="">
+                                <div class="flex justify-center">
+                                    <button class="">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                            class="size-10">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.75V12A2.25 2.25 0 0 1 4.5 9.75h15A2.25 2.25 0 0 1 21.75 12v.75m-8.69-6.44-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
+                                        </svg>
+
+                                    </button>
+                                </div>
+                                <p class="text-center">Dossier</p>
+                            </a>
+                        </li>
+
+                        <li class="block {{ (Route::is('')) ? 'text-white bg-green' : ''}} nav-item p4">
+                            <a class="hover:text-white" href="">
+                                <div class="flex justify-center">
+                                    <button class="">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                            class="size-10">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
+                                        </svg>
+
+                                    </button>
+                                </div>
+                                <p class="text-center">Agenda</p>
+                            </a>
+                        </li>
+
+                        <li class="block {{ (Route::is('')) ? 'text-white bg-green' : ''}} nav-item p4">
+                            <a class="hover:text-white" href="">
+                                <div class="flex justify-center">
+                                    <button class="">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                            class="size-10">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m3.75 9v7.5m2.25-6.466a9.016 9.016 0 0 0-3.461-.203c-.536.072-.974.478-1.021 1.017a4.559 4.559 0 0 0-.018.402c0 .464.336.844.775.994l2.95 1.012c.44.15.775.53.775.994 0 .136-.006.27-.018.402-.047.539-.485.945-1.021 1.017a9.077 9.077 0 0 1-3.461-.203M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+                                        </svg>
+
+                                    </button>
+                                </div>
+                                <p class="text-center">Factures</p>
+                            </a>
+                        </li>
+
+                        <li class="block {{ (Route::is('')) ? 'text-white bg-green' : ''}} nav-item p4">
+                            <a class="hover:text-white" href="">
+                                <div class="flex justify-center">
+                                    <button class="">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                            class="size-10">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 21h16.5M4.5 3h15M5.25 3v18m13.5-18v18M9 6.75h1.5m-1.5 3h1.5m-1.5 3h1.5m3-6H15m-1.5 3H15m-1.5 3H15M9 21v-3.375c0-.621.504-1.125 1.125-1.125h3.75c.621 0 1.125.504 1.125 1.125V21" />
+                                        </svg>
+
+                                    </button>
+                                </div>
+                                <p class="text-center">Clinique</p>
+                            </a>
+                        </li>
+
+                        <li class="block {{ (Route::is('')) ? 'text-white bg-green' : ''}} nav-item p4">
+                            <a class="hover:text-white" href="">
+                                <div class="flex justify-center">
+                                    <button class="">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                            class="size-10">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
+                                        </svg>
+                                    </button>
+                                </div>
+                                <p class="text-center">Collaborer</p>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </aside>
+
+            <nav class ="">
+                    <div class="">
+                        <div class="relative flex items-center justify-between">
+                            <div :class="{ 'hidden': open, 'ml-0': !open }" class="absolute inset-y-0 left-0 flex item-center md:hiden transition-all duration-300">
+                                <button type="button" @click="open = !open" @click.away="open = false" class="inline-flex rounded-md md:hidden bg-mid-green m-4 p-1 absolute text-darker-green hover:text-white focus:outline-none">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="block size-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                                    </svg>
+
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            <!-- Main content -->
+            <main :class="{ 'ml-32 md:ml-0': open, 'ml-10 md:ml-0': !open }" class="transition-all duration-300">
+                <div>
+                    {{ $slot}}
+                </div>
+            </main>
+
+        </div>
+    </body>
+</html>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="font-sans antialiased bg-pale-green">
+
+        <header>
+            <!-- Logo -->
+            <nav class="bg-dark-green p-2">
                 <div class="flex text-white items-center">
                     <img class="w-[5rem]" src="{{ asset('img/logoRdvSante.png') }}" alt="Logo">
                     <h1 class="ml-4 text-2xl font-extrabold">Rendez-vous santé</h1>
