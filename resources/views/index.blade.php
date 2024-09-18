c9a619c0ed6c96fef07f02642feec12f766b8a20
Maxime Malette <132301458+madmax9845@users.noreply.github.com>
2024-09-18 18:46:24 -0400

    Merge branch 'main' of https://github.com/AnthonyCarmichael/rdvSante

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,5 @@
 <x-admin-layout>
     <div class="container mx-auto p-4">
-        <h1 class="text-4xl font-bold mb-6">Bienvenue sur votre tableau de bord</h1>
+        <h2 class="text-4xl font-bold mb-6">Bienvenue sur votre tableau de bord</h2>
     </div>
 </x-admin-layout>
