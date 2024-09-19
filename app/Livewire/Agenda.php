17dbb5857e1af68e5f0c45a7bc4622e9d5f6f77e
AnthonyCarmichael <anthonycarmichael0@gmail.com>
2024-09-19 13:24:39 -0400

    update branche avec les modifs du main

--- app/Livewire/Agenda.php
+++ app/Livewire/Agenda.php
@@ -4,9 +4,26 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use Illuminate\Support\Facades\Log;
+use Carbon\Carbon;
 
 class Agenda extends Component
 {
+    public $view;
+    public $startingDate;
+
+    public function mount()
+    {
+        $this->view = "semaine";
+        $this->startingDate = Carbon::now()->format('d-M-Y');
+    }
+
+    public function setView($view)
+    {
+        if ($view !== $this->view) {
+            $this->view = $view;
+            $this->startingDate = Carbon::now()->format('d-M-Y');
+        }
+    }
 
     public function render()
     {
--- resources/views/livewire/agenda.blade.php
+++ resources/views/livewire/agenda.blade.php
@@ -1,6 +1,66 @@
 
 <div class="">
+    <div>
+        <button 
+            type="button" 
+            class="{{ $view === 'semaine' ? 'bg-green text-white' : 'bg-green text-darker-green hover:bg-green hover:text-white'}}"
+            wire:click="setView('semaine')"
+        >
+            Semaine
+        </button>
+
+        <button 
+            type="button" 
+            class="{{ $view === 'mois' ? 'bg-green text-white' : 'bg-green text-darker-green hover:bg-green hover:text-white'}}"
+            wire:click="setView('mois')"
+        >
+            Mois
+        </button>
+    </div>
+
+    <!-- Affichage de la vue sélectionnée -->
+    <div>
+        @if($view === 'semaine')
+            <!-- Affichage de la vue semaine -->
+            <p>Vue Semaine {{$startingDate}} </p>
+            
+        @elseif($view === 'mois')
+            <!-- Affichage de la vue mois -->
+            <p>Vue Mois {{$startingDate}}</p>
+        @endif
+    </div>
+
     
+    <table class="text-left table-fixed">
+        <thead>
+            <tr>
+                <!-- Titre col -->
+                <th>Heure</th>
+                <th>Lundi</th>
+                <th>Mardi</th>
+                <th>Mercredi</th>
+                <th>Jeudi</th>
+                <th>Vendredi</th>
+                <th>Samedi</th>
+                <th>Dimanche</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+        <tfoot>
+
+        </tfoot>
+    </table> 
 </div>
 
 
