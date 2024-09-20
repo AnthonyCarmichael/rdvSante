3fef05dabdd3619755d289aa21a4326c9b6d7b8b
Richard-Dongmo <dongmorichard01@gmail.com>
2024-09-20 08:59:48 -0400

    Installation de Laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,15 @@
 <?php
 
-use App\Http\Controllers\ClientController;
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\ServiceController;
-use App\Models\Service;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
-})->name('index');
+Route::view('/', 'welcome');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-Route::controller(ClientController::class)->group(function() {
-    Route::get('/Clients', 'index')->name('clients');
-});
-
-
-//Routes pour les services
-Route::controller(ServiceController::class)->group(function () {
-    Route::post('/service/ajouterService','store')->name('ajouterService');
-});
-
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
 
 require __DIR__.'/auth.php';
