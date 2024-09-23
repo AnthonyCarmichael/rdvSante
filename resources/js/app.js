1b4d1b8923f5bf75140ca41b82e08a265e5deabb
AnthonyCarmichael <anthonycarmichael0@gmail.com>
2024-09-22 21:59:08 -0400

    apres une éternité j'ai réussi a faire un modal

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,12 @@
 import './bootstrap';
 
 
+document.addEventListener('DOMContentLoaded', () => {
+    window.addEventListener('open-modal', event => {
+        let modalName = event.detail;
+        let modal = document.querySelector(`div[name="${modalName}"]`);
+        if (modal) {
+            modal.show = true; // Active le modal
+        }
+    });
+});
--- resources/views/livewire/indisponibilite-component.blade.php
+++ resources/views/livewire/indisponibilite-component.blade.php
@@ -1,11 +1,4 @@
 <div>
-    <form wire:submit.prevent="createIndisponibilite">
-        <input type="text" wire:model="note" placeholder="Note" required>
-        <input type="datetime-local" wire:model="dateHeureDebut" required>
-        <input type="datetime-local" wire:model="dateHeureFin" required>
-        <button type="submit">Ajouter Indisponibilité</button>
-    </form>
-
     <h3>Indisponibilités</h3>
     <ul>
         @foreach($indisponibilites as $indisponibilite)
