4a9d03f2a72d2fd69c1c018b5b7206d0516d90a2
AnthonyCarmichael <anthonycarmichael0@gmail.com>
2024-10-01 13:08:36 -0400

    mise en commentaire du switch de vue qui change les attribut for some reason

--- resources/views/livewire/agenda.blade.php
+++ resources/views/livewire/agenda.blade.php
@@ -1,5 +1,6 @@
 
 <div class="">
+    <!-- Bug avec la vue de mois qui change le now for some reason
     <div>
         <select id="view" name="view" wire:model="view" wire:change="setView($event.target.value)"
             class="border-none bg-mid-green">
@@ -12,6 +13,7 @@
         </div>
 
     </div>
+-->
 
     <!-- Affichage de la vue sélectionnée -->
     <div class="w-full text-gray-800">
