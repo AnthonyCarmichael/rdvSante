0971837c162d596968d1fa0c255780c9f03bd65d
Maxime Malette <132301458+madmax9845@users.noreply.github.com>
2024-09-30 23:03:27 -0400

    Gestion des dispos terminer

--- app/Livewire/GestionDispo.php
+++ app/Livewire/GestionDispo.php
@@ -11,10 +11,13 @@ use App\Models\Jour;
 
 class GestionDispo extends Component
 {
+    public $dispos;
     public $dispo;
     public $idDispo;
     public $jours;
+    public $journee;
     public $jour = '1';
+    public $nomJour;
     public $heureDebut;
     public $heureFin;
     public function render()
@@ -25,7 +28,7 @@ class GestionDispo extends Component
     protected function rules()
     {
         return [
-            'heureFin' => 'required',
+            'heureFin' => 'required|after:heureDebut',
             'heureDebut' => 'required',
             'jour' => 'required'
         ];
@@ -33,13 +36,13 @@ class GestionDispo extends Component
 
     public function mount()
     {
-        $this->dispo = Disponibilite::orderBy('heureDebut', 'asc')->findMany(DiponibiliteProfessionnel::where('id_user', '=', Auth::user()->id)->get());
+        $this->dispos = Disponibilite::orderBy('heureDebut', 'asc')->findMany(DiponibiliteProfessionnel::where('id_user', '=', Auth::user()->id)->get());
         $this->jours = Jour::all();
     }
 
     public function formAjout()
     {
-
+        $this->reset(['jour', 'heureDebut', 'heureFin']);
         $this->dispatch('open-modal', name: 'ajouterDispo');
 
     }
@@ -49,16 +52,16 @@ class GestionDispo extends Component
 
         $empietement = false;
 
-        foreach ($this->dispo as $d){
-            if ($d->idJour == $this->jour){
-                if ((($this->heureDebut >= $d->heureDebut && $this->heureDebut <= $d->heureFin) || ($this->heureFin >= $d->heureDebut && $this->heureFin <= $d->heureFin)) || (($d->heureDebut >= $this->heureDebut && $d->heureFin <= $this->heureDebut) || ($d->heureDebut >= $this->heureFin && $d->heureFin <= $this->heureFin))){
+        foreach ($this->dispos as $d) {
+            if ($d->idJour == $this->jour) {
+                if ((($this->heureDebut >= $d->heureDebut && $this->heureDebut <= $d->heureFin) || ($this->heureFin >= $d->heureDebut && $this->heureFin <= $d->heureFin)) || (($d->heureDebut >= $this->heureDebut && $d->heureFin <= $this->heureDebut) || ($d->heureDebut >= $this->heureFin && $d->heureFin <= $this->heureFin))) {
                     $empietement = true;
                     break;
                 }
             }
         }
 
-        if(!$empietement){
+        if (!$empietement) {
             $this->idDispo = Disponibilite::insertGetId([
                 'heureDebut' => $this->heureDebut,
                 'heureFin' => $this->heureFin,
@@ -73,8 +76,72 @@ class GestionDispo extends Component
 
 
         $this->reset(['jour', 'heureDebut', 'heureFin']);
-        $this->dispo = Disponibilite::orderBy('heureDebut', 'asc')->findMany(DiponibiliteProfessionnel::where('id_user', '=', Auth::user()->id)->get());
+        $this->dispos = Disponibilite::orderBy('heureDebut', 'asc')->findMany(DiponibiliteProfessionnel::where('id_user', '=', Auth::user()->id)->get());
         $this->jours = Jour::all();
         $this->dispatch('close-modal');
+    }
+
+    public function modifDispo()
+    {
+        $this->validate();
+
+        $empietement = false;
+
+        foreach ($this->dispos as $d) {
+            if ($d->idJour == $this->jour) {
+                if ((($this->heureDebut >= $d->heureDebut && $this->heureDebut <= $d->heureFin) || ($this->heureFin >= $d->heureDebut && $this->heureFin <= $d->heureFin)) || (($d->heureDebut >= $this->heureDebut && $d->heureFin <= $this->heureDebut) || ($d->heureDebut >= $this->heureFin && $d->heureFin <= $this->heureFin))) {
+                    $empietement = true;
+                    break;
+                }
+            }
+        }
+
+        if (!$empietement) {
+            Disponibilite::find($this->dispo->id)->update([
+                'heureDebut' => $this->heureDebut,
+                'heureFin' => $this->heureFin,
+                'idJour' => $this->jour
+            ]);
+        }
+
+        $this->reset(['jour', 'heureDebut', 'heureFin']);
+        $this->dispos = Disponibilite::orderBy('heureDebut', 'asc')->findMany(DiponibiliteProfessionnel::where('id_user', '=', Auth::user()->id)->get());
+        $this->dispatch('close-modal');
+        #exemple open modal dispatch
+        #$this->dispatch('open-modal', name: 'modal-name');
+
+    }
+
+    public function getInfoDispo($id)
+    {
+        $this->dispo = Disponibilite::find($id);
+        $this->heureDebut = $this->dispo->heureDebut;
+        $this->heureFin = $this->dispo->heureFin;
+        $this->jour = $this->dispo->idJour;
+        $this->dispatch('open-modal', name: 'modifierDispo');
+
+    }
+
+    public function confirmerSuppression($id)
+    {
+        $this->dispo = Disponibilite::find($id);
+        $this->heureDebut = $this->dispo->heureDebut;
+        $this->heureFin = $this->dispo->heureFin;
+        $this->journee = Jour::find($this->dispo->idJour);
+        $this->nomJour = $this->journee->nom;
+        $this->dispatch('open-modal', name: 'supprimerDispo');
+    }
+
+    public function supDispo()
+    {
+        DiponibiliteProfessionnel::where('idDisponibilite', '=', $this->dispo->id)->delete();
+        Disponibilite::where('id', '=', $this->dispo->id)->delete();
+
+        $this->reset(['dispo', 'jour', 'heureDebut', 'heureFin']);
+        $this->dispos = Disponibilite::orderBy('heureDebut', 'asc')->findMany(DiponibiliteProfessionnel::where('id_user', '=', Auth::user()->id)->get());
+        $this->dispatch('close-modal');
+        #exemple open modal dispatch
+        #$this->dispatch('open-modal', name: 'modal-name');
+
     }
 }
--- resources/views/livewire/gestion-dispo.blade.php
+++ resources/views/livewire/gestion-dispo.blade.php
@@ -19,24 +19,26 @@
                 <td class="bg-white align-top">
                     {{ $j->nom }}</td>
                 <td class="bg-white align-center">
-                    @foreach ($dispo as $d)
+                    @foreach ($dispos as $d)
                         @if ($d->idJour == $j->id)
-                        <div class="my-2">{{ $d->heureDebut }}</div>
+                            <div class="my-2">{{ $d->heureDebut }}</div>
                         @endif
                     @endforeach
                 </td>
                 <td class="bg-white">
-                    @foreach ($dispo as $d)
+                    @foreach ($dispos as $d)
                         @if ($d->idJour == $j->id)
-                        <div class="my-2">{{ $d->heureFin }}</div>
+                            <div class="my-2">{{ $d->heureFin }}</div>
                         @endif
                     @endforeach
                 </td>
                 <td class="bg-white">
-                    @foreach ($dispo as $d)
+                    @foreach ($dispos as $d)
                         @if ($d->idJour == $j->id)
-                            <button class="bg-selected-green mx-1 my-1 rounded p-0.5 w-1/2"
-                                type="button">Modifier</button>
+                            <button class="bg-selected-green mx-1 my-1 rounded p-0.5 w-5/12" type="button"
+                                wire:click="getInfoDispo({{ $d->id }})">Modifier</button>
+                            <button class="bg-selected-green mx-1 my-1 rounded p-0.5 w-5/12" type="button"
+                                wire:click="confirmerSuppression({{ $d->id }})">Supprimer</button>
                         @endif
                     @endforeach
                 </td>
@@ -46,24 +48,26 @@
                 <td class="bg-table-green align-top">
                     {{ $j->nom }}</td>
                 <td class="bg-table-green">
-                    @foreach ($dispo as $d)
+                    @foreach ($dispos as $d)
                         @if ($d->idJour == $j->id)
-                        <div class="my-2">{{ $d->heureDebut }}</div>
+                            <div class="my-2">{{ $d->heureDebut }}</div>
                         @endif
                     @endforeach
                 </td>
                 <td class="bg-table-green pr-4">
-                    @foreach ($dispo as $d)
+                    @foreach ($dispos as $d)
                         @if ($d->idJour == $j->id)
-                        <div class="my-2">{{ $d->heureFin }}</div>
+                            <div class="my-2">{{ $d->heureFin }}</div>
                         @endif
                     @endforeach
                 </td>
                 <td class="bg-table-green">
-                    @foreach ($dispo as $d)
+                    @foreach ($dispos as $d)
                         @if ($d->idJour == $j->id)
-                            <button class="bg-selected-green mx-1 my-1 rounded p-0.5 w-1/2"
-                                type="button">Modifier</button>
+                            <button class="bg-selected-green mx-1 my-1 rounded p-0.5 w-5/12" type="button"
+                                wire:click="getInfoDispo({{ $d->id }})">Modifier</button>
+                            <button class="bg-selected-green mx-1 my-1 rounded p-0.5 w-5/12" type="button"
+                                wire:click="confirmerSuppression({{ $d->id }})">Supprimer</button>
                         @endif
                     @endforeach
                 </td>
@@ -98,6 +102,47 @@
                             {{ $j->nom }}</option>
                     @endforeach
                 </select>
+
+            </div>
+            <div class="flex justify-center mt-4">
+                <button class="w-3/12 bg-selected-green mx-1 my-1 rounded p-0.5" type="submit">Confirmer</button>
+            </div>
+
+        </form>
+    </x-modal>
+
+    <x-modal title="Modifier une disponibilité" name="modifierDispo" :show="false">
+        <form wire:submit.prevent="modifDispo" class="bg-white p-4 rounded-lg">
+            <div class="grid grid-cols-4 gap-y-4">
+
+                <label class="text-right" for="heureDebut">Heure de début:*</label>
+                <input wire:model="heureDebut" class="ml-2" type="time" id="heureDebut" name="heureDebut" />
+
+                <label class="text-right" for="heureFin">Heure de fin:*</label>
+                <input wire:model="heureFin" class="ml-2" type="time" id="heureFin" name="heureFin" />
+
+
+                <label class="text-right" for="jour">Jour:*</label>
+                <select wire:model="jour" id="jour" name="jour" class="ml-2">
+                    @foreach ($jours as $j)
+                        <option value={{ $j->id }} {{ $j->id === $jour ? 'selected' : '' }}>
+                            {{ $j->nom }}</option>
+                    @endforeach
+                </select>
+
+            </div>
+            <div class="flex justify-center mt-4">
+                <button class="w-3/12 bg-selected-green mx-1 my-1 rounded p-0.5" type="submit">Confirmer</button>
+            </div>
+
+        </form>
+    </x-modal>
+
+    <x-modal title="Supprimer une disponibilité" name="supprimerDispo" :show="false">
+        <form wire:submit.prevent="supDispo" class="bg-white p-4 rounded-lg">
+            <div class="">
+                <p>Êtes vous sûr de vouloir supprimer cette disponibilité?</p><br>
+                <p>Le {{ $nomJour }} de {{ $heureDebut }} à {{ $heureFin }}. </p>
 
             </div>
             <div class="flex justify-center mt-4">
