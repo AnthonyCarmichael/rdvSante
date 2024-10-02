58ad9d06f84202cc3f05800853f73e852023445c
Anthony Carmichael <anthonycarmichael0@gmail.com>
2024-10-02 13:56:03 -0400

    modification du login

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,12 +15,33 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-mid-green">
+        <header>
+            <!-- Logo -->
+            <nav class="bg-dark-green p-2">
+                <div class="flex justify-between items-center">
+                    <div class="flex text-white items-center ml-0">
+                        <img class="w-[4rem]" src="{{ asset('img/logoRdvSante.png') }}" alt="Logo">
+                        <h1 class="ml-6 text-2xl font-extrabold">Rendez-vous santé</h1>
+                    </div>
+
+                    @auth
+                        <!-- Bouton de déconnexion -->
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            <button type="submit" class="text-white bg-mid-green hover:bg-red-700 px-4 py-2 rounded-md">
+                                Déconnexion
+                            </button>
+                        </form>
+                    @endauth
+                </div>
+            </nav>
+        </header>
+        <div class="min-h-screen flex flex-col items-center pt-16 bg-darker-green">
             <div>
-                <label class="text-3xl font-medium" for="Title">Connexion professionnel.le</label>
+                <label class="text-3xl mt-8 font-medium text-white" for="Title">Connexion</label>
             </div>
 
-            <div class="bg-green w-full sm:max-w-md mt-6 px-6 py-4 shadow-md overflow-hidden sm:rounded-lg border-solid border-2 border-black">
+            <div class="bg-green w-full sm:max-w-md mt-6 px-6 py-4 shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
         </div>
