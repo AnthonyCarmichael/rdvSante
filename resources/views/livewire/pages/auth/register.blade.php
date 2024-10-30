8bcadd16de04aa2ef2bb8b964bc3c187b59b0379
Richard-Dongmo <dongmorichard01@gmail.com>
2024-10-30 17:49:47 -0400

    MAJ du formulaire d'invitation

--- resources/views/livewire/pages/auth/register.blade.php
+++ resources/views/livewire/pages/auth/register.blade.php
@@ -74,42 +74,42 @@ new #[Layout('layouts.guest')] class extends Component
         <form wire:submit="register">
             <!-- Nom -->
             <div>
-                <x-input-label for="nom" :value="__('Nom')" />
+                <x-input-label for="nom" :value="__('Nom *')" />
                 <x-text-input wire:model="nom" id="nom" class="block mt-1 w-full" type="text" name="nom" required autofocus autocomplete="nom" />
                 <x-input-error :messages="$errors->get('nom')" class="mt-2" />
             </div>
 
             <!-- Prénom -->
             <div>
-                <x-input-label for="prenom" :value="__('Prénom')" />
+                <x-input-label for="prenom" :value="__('Prénom *')" />
                 <x-text-input wire:model="prenom" id="prenom" class="block mt-1 w-full" type="text" name="prenom" required autofocus autocomplete="prenom" />
                 <x-input-error :messages="$errors->get('prenom')" class="mt-2" />
             </div>
 
             <!-- Email -->
             <div class="mt-4">
-                <x-input-label for="email" :value="__('Email')" />
+                <x-input-label for="email" :value="__('Email *')" />
                 <x-text-input wire:model="email" id="email" class="block mt-1 w-full" type="email" name="email" required autocomplete="username" />
                 <x-input-error :messages="$errors->get('email')" class="mt-2" />
             </div>
 
             <!-- Téléphone -->
             <div class="mt-4">
-                <x-input-label for="telephone" :value="__('Téléphone')" />
+                <x-input-label for="telephone" :value="__('Téléphone *')" />
                 <x-text-input wire:model="telephone" id="telephone" class="block mt-1 w-full" type="text" name="telephone" required autocomplete="telephone" />
                 <x-input-error :messages="$errors->get('telephone')" class="mt-2" />
             </div>
 
             <!-- Mot de passe -->
             <div class="mt-4">
-                <x-input-label for="password" :value="__('Mot de passe')" />
+                <x-input-label for="password" :value="__('Mot de passe *')" />
                 <x-text-input wire:model="password" id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
                 <x-input-error :messages="$errors->get('password')" class="mt-2" />
             </div>
 
             <!-- Confirmer le mot de passe -->
             <div class="mt-4">
-                <x-input-label for="password_confirmation" :value="__('Confirmer le mot de passe')" />
+                <x-input-label for="password_confirmation" :value="__('Confirmer le mot de passe *')" />
                 <x-text-input wire:model="password_confirmation" id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
                 <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
             </div>
@@ -131,7 +131,7 @@ new #[Layout('layouts.guest')] class extends Component
             <!-- Soumettre -->
             <div class="flex items-center justify-end mt-4">
                 <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}" wire:navigate>
-                    {{ __('Already registered?') }}
+                    {{ __('Déjà inscrit?') }}
                 </a>
 
                 <x-primary-button class="ms-4">
