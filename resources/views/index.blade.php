634d0654591613ea712241ad2c2a907e00fc6079
Maxime Malette <132301458+madmax9845@users.noreply.github.com>
2024-10-30 17:54:14 -0400

    Correction bug pré-rencontre

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,21 +1,65 @@
+<?php
+use App\Models\Service;
+use App\Models\ProfessionProfessionnel;
+use App\Models\DiponibiliteProfessionnel;
+use App\Models\CliniqueProfessionnel;
+use Illuminate\Support\Facades\Auth;
+?>
 <x-admin-layout>
     <div class="container mx-auto p-4">
         <h2 class="text-4xl font-bold mb-6">Bienvenue sur votre tableau de bord</h2>
     </div>
     @php
+
         $photo = false;
         $service = false;
         $clinique = false;
         $description = false;
         $dispo = false;
+        $profession = false;
+
+        $services = Service::all();
+        $professionProfessionnel = ProfessionProfessionnel::all();
+        $dispoProfessionnel = DiponibiliteProfessionnel::all();
+        $cliniqueProfessionnel = CliniqueProfessionnel::all();
+
+        foreach ($services as $s) {
+            if ($s->idProfessionnel == Auth::user()->id) {
+                $service = true;
+            }
+        }
+        foreach ($professionProfessionnel as $p) {
+           if ($p->user_id == Auth::user()->id) {
+            $profession = true;
+           }
+        }
+        foreach ($dispoProfessionnel as $d) {
+           if ($d->id_user == Auth::user()->id) {
+            $dispo = true;
+           }
+        }
+        foreach ($cliniqueProfessionnel as $c) {
+           if ($c->idProfessionnel == Auth::user()->id) {
+            $clinique = true;
+           }
+        }
+        if (Auth::user()->description != null) {
+            $description = true;
+        }
+        if (file_exists("../public/img/icone_" . strval(Auth::user()->id) . ".jpg")) {
+            $photo = true;
+        }
     @endphp
-    @if (!$photo || !$service || !$clinique || !$description || !$dispo)
+    @if (!$photo || !$service || !$clinique || !$description || !$dispo || !$profession)
         <div class="container mx-auto p-4">
             <h3 class="text-2xl font-bold mb-6">Voici une liste de choses à faire pour officielement activé votre compte!
             </h3>
             <ul class="mx-auto p-4 text-xl">
                 @if (!$photo)
                     <li class="list-disc">Envoyer une photo à l'administrateur pour l'ajouter à votre profil</li>
+                @endif
+                @if (!$profession)
+                    <li class="list-disc">Ajouter au moins une profession à votre profil</li>
                 @endif
                 @if (!$service)
                     <li class="list-disc">Ajouter au moins un service</li>
