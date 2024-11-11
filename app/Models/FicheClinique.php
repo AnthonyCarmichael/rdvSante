43a1b6ef48d1e31b4b2ba8b1272cd12b50c5ba2e
Anthony Carmichael <anthonycarmichael0@gmail.com>
2024-11-11 12:33:23 -0500

    Modification dans la bd pour la table ficheClinique. Ajout des champs pour tout les type de fiche. Besoin de faire un migrate fresh

--- app/Models/FicheClinique.php
+++ app/Models/FicheClinique.php
@@ -8,4 +8,83 @@ use Illuminate\Database\Eloquent\Model;
 class FicheClinique extends Model
 {
     use HasFactory;
+
+    // Ajoute 'idDossier' à la propriété fillable
+    protected $fillable = [
+        'dateHeure',
+        'idDossier',
+        'idTypeFiche',
+        'idProfession',
+        'nom',
+        'description',
+        'conseilsPrevention',
+        'occupation',
+        'loisirs',
+        'lateralite',
+        'diagnostic',
+        'medic',
+        'contreIndication',
+        'rced',
+        'localIrr',
+        'douleur',
+        'fa',
+        'fd',
+        'nuit',
+        'sa',
+        'investigation',
+        'trauma',
+        'chx',
+        'familiaux',
+        'cardioVasculaire',
+        'pulmonaire',
+        'snc',
+        'orl',
+        'digestif',
+        'gynecoAndrologie',
+        'urinaire',
+        'hs',
+        'psychologique',
+        'msk',
+        'dermato',
+        'autre',
+        'observation',
+        'commentaire',
+        'nbreSemGestation',
+        'apgar',
+        'poid',
+        'taille',
+        'perCranien',
+        'maladieALaNaissance',
+        'medicaments',
+        'nomsParent',
+        'historiqueGrossesse',
+        'historiqueAccouchement',
+        'cesarienne',
+        'forceps',
+        'ventouse',
+        'episiotomie',
+        'alimentation',
+        'digestion',
+        'pleurs',
+        'motricite',
+        'neuro',
+        'motifConsultation',
+        'techniques',
+    ];
+
+    public $timestamps = false;
+
+    public function typeFiche()
+    {
+        return $this->belongsTo(TypeFiche::class, 'idTypeFiche');
+    }
+    public function dossier()
+    {
+        return $this->belongsTo(Dossier::class, 'idDossier');
+    }
+    public function profession()
+    {
+        return $this->belongsTo(Profession::class, 'idProfession');
+    }
+
 }
--- database/migrations/2024_09_12_154302_create_fiche_cliniques_table.php
+++ database/migrations/2024_09_12_154302_create_fiche_cliniques_table.php
@@ -18,6 +18,67 @@ return new class extends Migration
             $table->dateTime('dateHeure');
             $table->bigInteger('idTypeFiche')->unsigned();
             $table->bigInteger('idDossier')->unsigned();
+            $table->bigInteger('idProfession')->unsigned();
+
+            $table->string('nom')->nullable();
+            $table->text('description')->nullable();
+            $table->text('analyse')->nullable();
+            $table->text('conseilsPrevention')->nullable();
+            // Zone anamnèse
+            $table->text('occupation')->nullable();
+            $table->text('loisirs')->nullable();
+            $table->text('lateralite')->nullable();
+            $table->text('diagnostic')->nullable();
+            $table->text('medic')->nullable();
+            $table->text('contreIndication')->nullable();
+            $table->text('rced')->nullable();
+            $table->text('localIrr')->nullable();
+            $table->text('douleur')->nullable();
+            $table->text('fa')->nullable();
+            $table->text('fd')->nullable();
+            $table->text('nuit')->nullable();
+            $table->text('sa')->nullable();
+            $table->text('investigation')->nullable();
+            $table->text('trauma')->nullable();
+            $table->text('chx')->nullable();
+            $table->text('familiaux')->nullable();
+            $table->text('cardioVasculaire')->nullable();
+            $table->text('pulmonaire')->nullable();
+            $table->text('snc')->nullable();
+            $table->text('orl')->nullable();
+            $table->text('digestif')->nullable();
+            $table->text('gynecoAndrologie')->nullable();
+            $table->text('urinaire')->nullable();
+            $table->text('hs')->nullable();
+            $table->text('psychologique')->nullable();
+            $table->text('msk')->nullable();
+            $table->text('dermato')->nullable();
+            $table->text('autre')->nullable();
+            $table->text('observation')->nullable();
+            $table->text('commentaire')->nullable();
+            // Zone nourrisson
+            #$table->text('ddn')->nullable(); Déjà dans la bd client
+            $table->integer('nbreSemGestation')->nullable();
+            $table->text('apgar')->nullable();
+            $table->text('poid')->nullable();
+            $table->text('taille')->nullable();
+            $table->text('perCranien')->nullable();
+            $table->text('maladieALaNaissance')->nullable();
+            $table->text('medicaments')->nullable();
+            $table->text('nomsParent')->nullable();
+            $table->text('historiqueGrossesse')->nullable();
+            $table->text('historiqueAccouchement')->nullable();
+            $table->boolean('cesarienne')->nullable();
+            $table->boolean('forceps')->nullable();
+            $table->boolean('ventouse')->nullable();
+            $table->boolean('episiotomie')->nullable();
+            $table->text('alimentation')->nullable();
+            $table->text('digestion')->nullable();
+            $table->text('pleurs')->nullable();
+            $table->text('motricite')->nullable();
+            $table->text('neuro')->nullable();
+            $table->text('motifConsultation')->nullable();
+            $table->text('techniques')->nullable();
         });
 
         Schema::table('fiche_cliniques', function (Blueprint $table) {
@@ -26,6 +87,10 @@ return new class extends Migration
 
         Schema::table('fiche_cliniques', function (Blueprint $table) {
             $table->foreign('idDossier')->references('id')->on('dossiers');
+        });
+
+        Schema::table('fiche_cliniques', function (Blueprint $table) {
+            $table->foreign('idProfession')->references('id')->on('professions');
         });
     }
 
