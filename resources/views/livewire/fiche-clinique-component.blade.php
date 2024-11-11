e36939113a81b562a8b712e4a4cb95b12f020bab
Anthony Carmichael <anthonycarmichael0@gmail.com>
2024-11-11 16:15:17 -0500

    début de form anamnese

--- resources/views/livewire/fiche-clinique-component.blade.php
+++ resources/views/livewire/fiche-clinique-component.blade.php
@@ -32,7 +32,7 @@
                     <fieldset class="">
                         <legend class="text-lg">Information client</legend>
                         <div class="flex">
-                            <div class="m-4">
+                            <div class="m-4 w-1/3">
                                 <label class="block text-sm" for="occupation">Occupation/travail</label>
                                 <textarea name="occupation" id="occupation"
                                 class="mt-1 text-black block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
@@ -40,7 +40,7 @@
 
                             </div>
 
-                            <div class="m-4">
+                            <div class="m-4 w-1/3">
                                 <label class="block text-sm" for="loisirs">Sports et loisirs</label>
                                 <textarea name="loisirs" id="loisirs"
                                 class="mt-1 text-black block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
@@ -48,7 +48,7 @@
 
                             </div>
 
-                            <div class="m-4">
+                            <div class="m-4 w-1/3">
                                 <label class="block text-sm" for="lateralite">Latéralité</label>
                                 <textarea name="lateralite" id="lateralite"
                                 class="mt-1 text-black block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
@@ -60,7 +60,7 @@
 
 
                         <div class="flex">
-                            <div class="m-4">
+                            <div class="m-4 w-1/3">
                                 <label class="block text-sm" for="diagnostic">Diagnostic</label>
                                 <textarea name="diagnostic" id="diagnostic"
                                 class="mt-1 text-black block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
@@ -68,7 +68,7 @@
 
                             </div>
 
-                            <div class="m-4">
+                            <div class="m-4 w-1/3">
                                 <label class="block text-sm" for="medic">Médic</label>
                                 <textarea name="loisirs" id="loisirs"
                                 class="mt-1 text-black block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
@@ -76,7 +76,7 @@
 
                             </div>
 
-                            <div class="m-4">
+                            <div class="m-4 w-1/3">
                                 <label class="block text-sm" for="contreIndication">Contre indications</label>
                                 <textarea name="contreIndication" id="contreIndication"
                                 class="mt-1 text-black block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
@@ -85,9 +85,69 @@
 
                         </div>
 
-
-                        <!-- more form input elements -->
                     </fieldset>
+                </div>
+
+                <div class=" text-sm flex border-t">
+                    <div class="w-1/2 border-r p-6">
+                        <fieldset class="">
+                            <legend class="text-lg">Raison de consultation client</legend>
+
+                            <div class="flex m-4 justify-between">
+                                <label class="block text-sm w-1/3" for="contreIndication">RC et ED</label>
+                                <textarea name="contreIndication" id="contreIndication"
+                                class="w-2/3 mt-1 text-black block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                rows="1"></textarea>
+                            </div>
+
+                            <div class="flex  m-4">
+                                <label class="block text-sm w-1/3" for="contreIndication">Local et irr</label>
+                                <textarea name="contreIndication" id="contreIndication"
+                                class="w-2/3 mt-1 text-black block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                rows="1"></textarea>
+                            </div>
+                            <div class="flex  m-4">
+                                <label class="block text-sm w-1/3" for="contreIndication">Douleur (Type, intensité, évolution)</label>
+                                <textarea name="contreIndication" id="contreIndication"
+                                class="w-2/3 mt-1 text-black block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                rows="1"></textarea>
+                            </div>
+                            <div class="flex  m-4">
+                                <label class="block text-sm w-1/3" for="contreIndication">FA</label>
+                                <textarea name="contreIndication" id="contreIndication"
+                                class="w-2/3 mt-1 text-black block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                rows="1"></textarea>
+                            </div>
+                            <div class="flex  m-4">
+                                <label class="block text-sm w-1/3" for="contreIndication">FD</label>
+                                <textarea name="contreIndication" id="contreIndication"
+                                class="w-2/3 mt-1 text-black block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                rows="1"></textarea>
+                            </div>
+                            <div class="flex  m-4">
+                                <label class="block text-sm w-1/3" for="contreIndication">Nuit</label>
+                                <textarea name="contreIndication" id="contreIndication"
+                                class="w-2/3 mt-1 text-black block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                rows="1"></textarea>
+                            </div>
+                            <div class="flex  m-4">
+                                <label class="block text-sm w-1/3" for="contreIndication">SA</label>
+                                <textarea name="contreIndication" id="contreIndication"
+                                class="w-2/3 mt-1 text-black block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                rows="1"></textarea>
+                            </div>
+                        </fieldset>
+                    </div>
+
+                    <div class="w-1/2 p-6">
+                        <fieldset class="">
+                            <legend class="text-lg">Investigation</legend>
+                            <textarea name="contreIndication" id="contreIndication"
+                            class="mt-1 text-black block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                            rows="5"></textarea>
+                        </fieldset>
+                    </div>
+
                 </div>
 
 
