522f6f2196c37638dc73b04c68b821d8d9e4a76a
Anthony Carmichael <anthonycarmichael0@gmail.com>
2024-11-13 14:47:33 -0500

    correction model dossier

--- app/Models/Dossier.php
+++ app/Models/Dossier.php
@@ -17,7 +17,7 @@ class Dossier extends Model
 
     public function professionnels(): BelongsToMany
     {
-        return $this->belongsToMany(User::class, 'dossier_professionnels','idDossier','user_id');
+        return $this->belongsToMany(User::class, 'dossier_professionnels','idDossier','idProfessionnel');
     }
 
     public function client()
