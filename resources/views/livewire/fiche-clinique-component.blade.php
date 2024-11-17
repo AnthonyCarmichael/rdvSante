fe3a94ac112aef2b5ab06746387778f0954762d0
AnthonyCarmichael <anthonycarmichael0@gmail.com>
2024-11-17 13:09:22 -0500

    textArea s'ajuste automatiquement selon leur contenu

--- resources/views/livewire/fiche-clinique-component.blade.php
+++ resources/views/livewire/fiche-clinique-component.blade.php
@@ -822,21 +822,6 @@
 </section>
 
 <script>
-document.addEventListener('DOMContentLoaded', function() {
-    // Ajuste la hauteur de tous les textarea au chargement de la page
-    setTimeout(() => {
-        document.querySelectorAll('.auto-resize').forEach(textarea => {
-            autoResize(textarea); // Ajuste la hauteur immédiatement en fonction du contenu initial
-            textarea.addEventListener('input', function() {
-                autoResize(textarea); // Ajuste la hauteur pendant la saisie
-            });
-        });
-    }, 0); // Délai de 0 millisecondes
-});
 
-function autoResize(textarea) {
-    textarea.style.height = 'auto'; // Réinitialise la hauteur avant de recalculer
-    textarea.style.height = textarea.scrollHeight + 'px'; // Ajuste la hauteur en fonction du contenu
-}
 
 </script>
